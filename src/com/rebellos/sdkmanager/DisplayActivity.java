a246728c5a1f6ec69a465dd823caee79c4fa7245
Dominik Marszk <dmarszk@gmail.com>
2013-05-11 19:17:41 +0200

    Applied GPL license.

--- src/com/rebellos/sdkmanager/DisplayActivity.java
+++ src/com/rebellos/sdkmanager/DisplayActivity.java
@@ -1,3 +1,23 @@
+/*
+This file is part of SDK Manager.
+
+Copyright 2013 Dominik Marszk <dmarszk@gmail.com>
+
+SDK Manager is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+SDK Manager is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SDK Manager; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 package com.rebellos.sdkmanager;
 
 import android.annotation.*;
